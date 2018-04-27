13c40b71fc7a565fcb501b7dbb793bfd9412ba94
Unknown <ms9642@nyu.edu>
2018-04-27 12:40:55 -0400

    Can now move between menu and game

--- Assets/Scripts/ToGameScene.cs
+++ Assets/Scripts/ToGameScene.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToGameScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+	public void PlayGame() {
+		SceneManager.LoadScene (1); //if the button is pressed, go to the game
 	}
 }
