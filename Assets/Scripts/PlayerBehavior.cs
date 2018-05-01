d406db5140c0dd1e999c6ed1c3cf0360bf9dd3ee
Unknown <snt265@nyu.edu>
2018-05-01 09:56:19 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	public float speed; // speed of player
+	private float xAxis; // move right when 1 move left when -1
+	private float yAxis; // move up when 1 move down when -1
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		speed = 3;
+		rb = this.GetComponent<Rigidbody2D> ();
+		rb.bodyType = RigidbodyType2D.Dynamic; 
+		rb.gravityScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xAxis = Input.GetAxis ("Horizontal"); // handle direction for A/D/left/right key presses
+		yAxis = Input.GetAxis ("Vertical"); // handle direction for W/S/up/down key presses 
+
+		transform.Translate (new Vector2 (xAxis, yAxis) * Time.deltaTime * speed);  // moves player
+	}
+}
