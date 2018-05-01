c1e173e45a1919f95ddc4ca99fc31a3ed20317bd
Unknown <ms9642@nyu.edu>
2018-05-01 10:26:40 -0400

    Added Story

---
+++ Assets/Scripts/ToStoryGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ToStoryGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void PlayGame() {
+		SceneManager.LoadScene (3); //if the button is pressed, go to the story
+	}
+}
